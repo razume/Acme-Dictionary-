c041e16f22ce5b32146a6677bce8fb9cc6e86824
Leighton A Pitman <leightonpitman14@gmail.com>
2020-02-22 12:16:19 -0500

    complete workshop

--- db.js
+++ db.js
@@ -1,27 +1,109 @@
-const pg = require("pg");
-const uuid = require("uuid");
+const pg = require('pg');
+const uuid = require('uuid');
 const { Client } = pg;
 
-const client = new Client("postgres://localhost/dictionary_db");
+const client = new Client('postgres://localhost/dictionary_db');
 
 client.connect();
 
 const sync = async () => {
   const SQL = `
+  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
   DROP TABLE IF EXISTS nouns;
-    CREATE TABLE nouns(
-      id uuid PRIMARY KEY,
-      name VARCHAR(100) NOT NULL UNIQUE
-);
-INSERT INTO nouns(id, name) VALUES("a863174e-57ed-4616-9d92-237b3ef7595a", "Table");
+  DROP TABLE IF EXISTS verbs;
+  DROP TABLE IF EXISTS adjectives;
+  CREATE TABLE nouns
+  (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR NOT NULL UNIQUE,
+    CHECK (char_length(name) > 0)
+  );
+  CREATE TABLE verbs
+  (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR NOT NULL UNIQUE,
+    CHECK (char_length(name) > 0)
+  );
+  CREATE TABLE adjectives
+  (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR NOT NULL UNIQUE,
+    CHECK (char_length(name) > 0)
+  );
 
-    `;
+  INSERT INTO nouns (name) VALUES ('table');
+  INSERT INTO verbs (name) VALUES ('run');
+  INSERT INTO adjectives (name) VALUES ('fast');
+
+
+  `;
+  await client.query(SQL);
+};
+
+const readNouns = async () => {
+  const SQL = 'SELECT * FROM nouns';
   const response = await client.query(SQL);
-  return response;
+  return response.rows;
+};
+
+const createNoun = async noun => {
+  const SQL = 'INSERT INTO nouns (name) VALUES ($1) returning *';
+  const response = await client.query(SQL, [noun]);
+  console.log(response.rows);
+  return response.rows[0];
+};
+
+const readVerbs = async () => {
+  const SQL = 'SELECT * FROM verbs';
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const createVerb = async noun => {
+  const SQL = 'INSERT INTO verbs (name) VALUES ($1) returning *';
+  const response = await client.query(SQL, [noun]);
+  return response.rows[0];
+};
+
+const readAdjectives = async () => {
+  const SQL = 'SELECT * FROM adjectives';
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const createAdjective = async noun => {
+  const SQL = 'INSERT INTO adjectives (name) VALUES ($1) returning *';
+  const response = await client.query(SQL, [noun]);
+  console.log(response.rows);
+  return response.rows[0];
+};
+
+const deleteNoun = async id => {
+  const SQL = 'DELETE FROM nouns WHERE id = $1';
+  await client.query(SQL, [id]);
+};
+
+const deleteVerb = async id => {
+  const SQL = 'DELETE FROM verbs WHERE id = $1';
+  await client.query(SQL, [id]);
+};
+
+const deleteAdjective = async id => {
+  const SQL = 'DELETE FROM adjectives WHERE id = $1';
+  await client.query(SQL, [id]);
 };
 
 sync();
 
 module.exports = {
-  sync
+  sync,
+  readNouns,
+  createNoun,
+  readVerbs,
+  createVerb,
+  readAdjectives,
+  createAdjective,
+  deleteNoun,
+  deleteVerb,
+  deleteAdjective
 };
--- server.js
+++ server.js
@@ -1,23 +1,92 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const db = require("./db");
-const morgan = require("morgan");
-const faker = require("faker");
-const path = require("path");
+const db = require('./db');
+const morgan = require('morgan');
+const faker = require('faker');
+const path = require('path');
 // const pg = require("pg");
 
 app.use(express.json());
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms")
+  morgan(':method :url :status :res[content-length] - :response-time ms')
 );
-app.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get("/api/nouns", (req, res, next) => {
-  console.log("nouns get");
+app.get('/api/nouns', (req, res, next) => {
+  db.readNoun()
+    .then(response => res.send(response))
+    .catch(next);
 });
+
+app.get('/api/verbs', (req, res, next) => {
+  db.readVerbs()
+    .then(response => res.send(response))
+    .catch(next);
+});
+
+app.get('/api/adjectives', (req, res, next) => {
+  db.readAdjectives()
+    .then(response => res.send(response))
+    .catch(next);
+});
+
+app.post('/api/nouns', async (req, res, next) => {
+  try {
+    const noun = await db.createNoun(faker.hacker.noun());
+    res.send(noun);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.post('/api/verbs', async (req, res, next) => {
+  try {
+    const verb = await db.createVerb(faker.hacker.verb());
+    res.send(verb);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.post('/api/adjectives', async (req, res, next) => {
+  try {
+    const adjective = await db.createAdjective(faker.hacker.adjective());
+    res.send(adjective);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.delete('/api/nouns/:id', async (req, res, next) => {
+  try {
+    await db.deleteNoun(req.params.id);
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.delete('/api/verbs/:id', async (req, res, next) => {
+  try {
+    await db.deleteVerb(req.params.id);
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.delete('/api/adjectives/:id', async (req, res, next) => {
+  try {
+    await db.deleteAdjective(req.params.id);
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
 app.use((err, req, res, next) => {
   res
     .status(err.status || 500)
@@ -27,8 +96,8 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 3000;
 
 db.sync().then(() => {
-  console.log("synced");
+  console.log('synced');
   app.listen(port, () => console.log(`listening on port ${port}`));
 });
 
-app.use(express.static("assets"));
+app.use(express.static('assets'));
