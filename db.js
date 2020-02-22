b3795e861b33ab6e7b5e346af6a102608ec86d11
Robert Celestino <panda@Roberts-MacBook-Air-2.local>
2020-02-22 10:46:17 -0500

    sql qont work

--- db.js
+++ db.js
@@ -2,12 +2,22 @@ const pg = require("pg");
 const uuid = require("uuid");
 const { Client } = pg;
 
+const client = new Client("postgres://localhost/dictionary_db");
+
 client.connect();
 
 const sync = async () => {
   const SQL = `
-    
+  DROP TABLE IF EXISTS nouns;
+    CREATE TABLE nouns(
+      id uuid PRIMARY KEY,
+      name VARCHAR(100) NOT NULL UNIQUE
+);
+INSERT INTO nouns(id, name) VALUES("a863174e-57ed-4616-9d92-237b3ef7595a", "Table");
+
     `;
+  const response = await client.query(SQL);
+  return response;
 };
 
 sync();
--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const app = express();
 const db = require("./db");
 const morgan = require("morgan");
 const faker = require("faker");
-const pg = require("pg");
+const path = require("path");
+// const pg = require("pg");
 
 app.use(express.json());
 
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api/nouns", (req, res, next) => {
+  console.log("nouns get");
+});
 app.use((err, req, res, next) => {
   res
     .status(err.status || 500)
@@ -22,3 +30,5 @@ db.sync().then(() => {
   console.log("synced");
   app.listen(port, () => console.log(`listening on port ${port}`));
 });
+
+app.use(express.static("assets"));
